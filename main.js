4d8caf97e51d73a4c14277614fd054c18f800b65
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-04 15:43:11 +0200

    arrow-list-test-and-for-loop

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ prima con un ciclo for, poi con un ciclo while.
 Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato,
 utilizzando uno dei due cicli scritti al punto precedente.
 */
+
+// ARROW
+let marketList = ["Test", "Test", "Test"];
+
+// FOR LOOP
+for (let i=0; i<marketList.length; i++){
+    console.log(marketList);
+    break;
+}
+
+
