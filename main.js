29b5e1d2e138a5c4349d738124db594c1d265418
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-04 15:34:27 +0200

    creation-folder-and-files-html-js

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+Data una lista della spesa, stampare in console gli elementi della lista individualmente,
+prima con un ciclo for, poi con un ciclo while.
+Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato,
+utilizzando uno dei due cicli scritti al punto precedente.
+*/
